c37a6ab6211d41bc83353a908e5fccd464ddbc86
Shuaiwei Yu <yu.shuaiwei.1999@gmail.com>
2026-02-07 21:34:16 +0100

    update Docusaurus configuration for GitHub Pages deployment

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://shuaiweiyu.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/QRPick/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ShuaiweiYu', // Usually your GitHub org/user name.
   projectName: 'QRPick', // Usually your repo name.
+  deploymentBranch: 'pages',
 
   onBrokenLinks: 'throw',
 
